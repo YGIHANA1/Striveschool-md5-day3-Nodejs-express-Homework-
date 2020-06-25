11935108851503fd205a34646e1909a5e2862853
Yannick.Gi <61831175+YGIHANA1@users.noreply.github.com>
2020-06-25 15:45:37 +0200

    add post

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express= require("express")
-
+const studentRoutes=require("./services/students")
 const server = express()
 
+server.use(express.json())
+server.use("/students",studentRoutes)
+
 const port = process.env.PORT
+
+
+
 
 server.listen(port,() =>{
     console.log(`server is running on port 3004: ${port}`)
